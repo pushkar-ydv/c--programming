5fb55d36fbc32d47d44509bccd662ccc52f099a7
pushkar-ydv <jpyadav73515@gmail.com>
2025-10-13 06:53:37 -0700

    Add files via upload

---
+++ LogicalOperators.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+
+    // int temp = 0;
+
+    // if(temp > 0 && temp < 30){
+        // printf("The temperature is GOOD");
+    // }
+    // else{
+    //    printf("The temperature is BAD");
+    // }
+    // int temp = 0;
+
+    // if(temp <= 0 || temp >= 30){
+        // printf("The temperature is BAD");
+    // }
+    // else{
+    //    printf("The temperature is GOOD");
+    // }
+    
+    bool isSunny = false;
+    
+    if(!isSunny){
+        printf("It is CLOUDY  outside");
+    }
+    else{
+        printf("It is SUNNY outside");
+    }
+
+
+    return 0;
+}
